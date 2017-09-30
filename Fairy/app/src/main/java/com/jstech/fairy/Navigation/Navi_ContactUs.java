c70ff713590eae113572b2893b7f842fa43bc5ad
ParkJinGi <rlqkrwls@naver.com>
2017-10-01 02:58:51 +0900

    Email 보내기 v1 구현 완료. 피드백 바람.

--- Fairy/app/src/main/java/com/jstech/fairy/Navigation/Navi_ContactUs.java
+++ Fairy/app/src/main/java/com/jstech/fairy/Navigation/Navi_ContactUs.java
@@ -1,38 +1,76 @@
 package com.jstech.fairy.Navigation;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.jstech.fairy.R;
 
 public class Navi_ContactUs extends AppCompatActivity {
-
+    ArrayAdapter Developers_List_Adapter;
     String UserTitle;
-    String UserEmail;
-    String TextArea;
+    String Receiver;
+    String EmailBody;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi__contact_us);
+
+        // 콤보박스와 리스트 배열 연결하기
+        Spinner Developer_List = (Spinner)findViewById(R.id.Developer_List);
+        Developers_List_Adapter = ArrayAdapter.createFromResource(this,R.array.Developers,android.R.layout.simple_spinner_item);
+        Developers_List_Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Developer_List.setAdapter(Developers_List_Adapter);
+
+        //리스너 부탁
+        Developer_List.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 1)
+                    Receiver = "Sujinsub@naver.com";
+                else if(position == 2)
+                    Receiver = "rlqkrwls@naver.com";
+                else if(position ==  3)
+                    Receiver = "ehfkddl29@naver.com";
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Receiver = null;
+            }
+        });
     }
     public void SendEmail(View v){
-        EditText EmailTitle = (EditText) findViewById(R.id.EmailTitle) ;
+        EditText EmailTitle = (EditText) findViewById(R.id.EmailTitle) ; // 메일 제목 받아오기
         UserTitle = EmailTitle.getText().toString();
+        EditText EmailText = (EditText) findViewById(R.id.EmailText) ; // 메일 본문 받아오기
+        EmailBody = EmailText.getText().toString();
 
-        EditText EmailString = (EditText) findViewById(R.id.EmailString) ;
-        UserEmail = EmailString.getText().toString();
+        if(UserTitle.trim().getBytes().length <= 0) { // 공백 또는 아무 내용이 없을 때
+            Toast.makeText(getApplicationContext(), "Please fill in Title", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-        EditText EmailText = (EditText) findViewById(R.id.EmailText) ;
-        TextArea = EmailText.getText().toString();
+        if(Receiver == null) { // 받는 사람 선택안했을 때
+            Toast.makeText(getApplicationContext(), "Please Select Receiver", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-        Toast.makeText(getApplicationContext(), UserTitle+"   "+UserEmail+"   "+"   "+TextArea, Toast.LENGTH_LONG).show();
+        if(EmailBody.trim().getBytes().length <= 0){ // 공백 또는 아무 내용이 없을 때
+            Toast.makeText(getApplicationContext(), "Please fill in Body", Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",Receiver, null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, UserTitle);
+        intent.putExtra(Intent.EXTRA_TEXT , EmailBody);
+        startActivity(Intent.createChooser(intent, "Send mail..."));
     }
-
-
-
 
 }
