87e33fe337527809d8287365a3834c06bb51eb19
ParkJinGi <rlqkrwls@naver.com>
2017-10-01 23:18:35 +0900

    1. 어플 잠금 스위치의 정보를 메인엑티비티와 공휴하기 2. 어플이 꺼졌다 켜져도 전에 있던 정보 유지하기

--- Fairy/app/src/main/java/com/jstech/fairy/Navigation/Navi_Secuity.java
+++ Fairy/app/src/main/java/com/jstech/fairy/Navigation/Navi_Secuity.java
@@ -1,15 +1,50 @@
 package com.jstech.fairy.Navigation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.jstech.fairy.R;
 
-public class Navi_Secuity extends AppCompatActivity {
+import static com.jstech.fairy.R.id.SecureSwitch;
 
+public class Navi_Secuity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private int SwitchStatement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi__secuity);
+
+        Intent intent = getIntent();
+        SwitchStatement = intent.getIntExtra("isSecure",0); // 현재 암호잠금 여부를 받아온다. default 값은 0이지만 0이 전달될 일이 없다.
+        Switch IsSecure = (Switch) findViewById(SecureSwitch);
+
+        if(SwitchStatement == 1) // OFF일 때
+            IsSecure.setChecked(false);
+        else
+            IsSecure.setChecked(true);
+        IsSecure.setOnCheckedChangeListener(this);
     }
+
+    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+        if (isChecked) {
+
+
+            
+
+
+            Intent intent = new Intent();
+            intent.putExtra("IsChanged",-1);
+            setResult(RESULT_OK,intent);
+        }else {
+            Intent intent = new Intent();
+            intent.putExtra("IsChanged",1);
+            setResult(RESULT_OK,intent);
+        }
+
+    }
+
 }
