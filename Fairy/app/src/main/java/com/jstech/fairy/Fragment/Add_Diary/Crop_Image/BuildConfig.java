c91d3859dfa9a969957c5264665f04f94c69acf7
ParkJinGi <rlqkrwls@naver.com>
2017-10-16 21:54:39 +0900

    Crop Image Library 이식

---
+++ Fairy/app/src/main/java/com/jstech/fairy/Fragment/Add_Diary/Crop_Image/BuildConfig.java
@@ -0,0 +1,17 @@
+package com.jstech.fairy.Fragment.Add_Diary.Crop_Image;
+
+/**
+ * Created by 박PC on 2017-10-16.
+ */
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+    public static final String APPLICATION_ID = "com.lyft.android.scissors";
+    public static final String BUILD_TYPE = "release";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = -1;
+    public static final String VERSION_NAME = "";
+
+    public BuildConfig() {
+    }
+}
