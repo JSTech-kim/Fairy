9df37349303eaae6665fdafd5adabc1b343525a5
ParkJinGi <rlqkrwls@naver.com>
2017-10-17 22:16:45 +0900

    사진 자르는 사이즈 정확하게 맞춤

--- Fairy/app/src/main/java/com/jstech/fairy/Fragment/Add_Diary/Crop.java
+++ Fairy/app/src/main/java/com/jstech/fairy/Fragment/Add_Diary/Crop.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import com.jstech.fairy.Fragment.Add_Diary.Crop_Image_Library.CropView;
@@ -32,6 +33,16 @@ public class Crop extends AppCompatActivity {
         Rotate_Button = (FloatingActionButton)findViewById(R.id.Rotate_fab);
 
         final CropView cropView = (CropView)findViewById(R.id.Crop_CropView);
+
+        /*                        네모 비율을 완벽하게 레이아웃과 맞춤                    */
+        DisplayMetrics dm = getApplicationContext().getResources().getDisplayMetrics();
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        float HeightRatio = (float)(height*0.3333334);
+        float WidthRatio = width;
+        cropView.setViewportRatio(WidthRatio/HeightRatio);
+        /*                       오차율 0.00000몇 %                                        */
+
         Intent intent = getIntent();
         galleryPictureUri = intent.getParcelableExtra("Uri");
         try {
@@ -53,6 +64,7 @@ public class Crop extends AppCompatActivity {
         Crop_Button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 bitmap = cropView.crop();
+
 
                 Intent data = new Intent();
                 setResult(RESULT_OK,data);
